11843231cc62600e3a4f6c141ce1d602cdf21e44
alousius <alousiusambrose18@gmail.com>
2023-08-17 22:46:40 +0530

    first commit

---
+++ src/main/java/com/oil/ecommerceoilapp/EcommerceOilAppApplication.java
@@ -0,0 +1,13 @@
+package com.oil.ecommerceoilapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOilAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOilAppApplication.class, args);
+	}
+
+}
