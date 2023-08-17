dc14d84c8b8c032f5425bc91a9caeec102601c07
Alousius <alousiusambrose18@gmail.com>
2023-08-17 23:17:11 +0530

    product modified

--- src/main/java/com/oil/ecommerceoilapp/entity/Product.java
+++ src/main/java/com/oil/ecommerceoilapp/entity/Product.java
@@ -1,4 +1,15 @@
 package com.oil.ecommerceoilapp.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
 public class Product {
 }
