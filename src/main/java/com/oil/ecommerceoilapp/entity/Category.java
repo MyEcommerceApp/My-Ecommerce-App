40633b153d1da5e560dc224485f5ea387de29464
Alousius <alousiusambrose18@gmail.com>
2023-08-17 23:13:43 +0530

    category properties add

--- src/main/java/com/oil/ecommerceoilapp/entity/Category.java
+++ src/main/java/com/oil/ecommerceoilapp/entity/Category.java
@@ -1,4 +1,32 @@
 package com.oil.ecommerceoilapp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "title", nullable = false, length = 155)
+    private String title;
+
+    @Column(name = "image")
+    private String imageURL;
+
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.ALL, CascadeType.PERSIST})
+    private List<Product> products;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled;
 }
